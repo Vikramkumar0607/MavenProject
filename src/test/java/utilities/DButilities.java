d09d97c1afbea34f450f7222ca29e294e988ec4d
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-07-02 23:44:56 +0530

    New changes

---
+++ src/test/java/utilities/DButilities.java
@@ -0,0 +1,18 @@
+package utilities;
+
+import java.util.List;
+import java.util.Map;
+
+public class DButilities {
+	
+	public static List<Map<String, String>> dbconnection(String fileName, String sheetName){
+	
+		
+		
+		
+		
+		return null;
+		
+	}
+
+}
